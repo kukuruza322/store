e517e7c5c4ece92ac47f3f637a35c345b2e3fd93
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2023-02-10 18:43:34 +0300

    creating product and price objects, simple checkout implemented

--- store/app/views.py
+++ store/app/views.py
@@ -1,3 +1,4 @@
+import stripe
 from django.shortcuts import render
 from django.views import generic
 
@@ -18,12 +19,42 @@ class DetailView(generic.DetailView):
 
 
 def buy(request, pk):
-    if request.method == "POST":
-        context = {'item_id': pk,
-                   }
+    item = Item.objects.get(pk=pk)
+    session = create_checkout_session(item)
+    context = {'item_id': pk,
+               'session_id': session,
+               }
+    if request.method == "GET":
         return render(request, 'app/buy.html', context=context)
     else:
         return render(request, 'app/index.html', context=context)
+
+
+def create_checkout_session(item):
+    stripe.api_key = 'sk_test_51MZw3sGWkXptNC3XZjlcZQM4nRZUnhDLwhKMtPNGYhotxXKHZFksmuwxWCCN3Gp0HQCdnX6YjBbQrdQrqPXS7XOd00Ci0ku2Fk'
+    product = stripe.Product.create(name=item.name,
+                                    )
+
+    price = stripe.Price.create(
+                              product=product.id,
+                              unit_amount=100*item.price,
+                              currency=item.currency.lower(),
+                            )
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': price,
+                    'quantity': 1,
+                },
+            ],
+            currency=item.currency.lower(),
+            mode='payment',
+            success_url='http://localhost:8000/success.html',
+        )
+        return checkout_session.url
+    except Exception as error:
+        return str(error)
 
 
 def success(request):
