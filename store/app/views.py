fb476d3d375e751eaab7942751efae697b20f1ee
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2023-02-11 14:42:52 +0300

    tax calc + refactoring

--- store/app/views.py
+++ store/app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic
 from django.views.decorators.http import require_GET
+import random
 
 from .models import Item
 
@@ -27,7 +28,6 @@ def buy(request, pk):
     """
     item = Item.objects.get(pk=pk)
     session = create_checkout_session(item)
-    print(session)
     return HttpResponseRedirect(session.url)
 
 
@@ -36,52 +36,60 @@ def create_checkout_session(item):
     Создает Stripe-объекты на основе количества товаров из корзины, выбранной страны и валюты.
     """
     stripe.api_key = 'sk_test_51MZw3sGWkXptNC3XZjlcZQM4nRZUnhDLwhKMtPNGYhotxXKHZFksmuwxWCCN3Gp0HQCdnX6YjBbQrdQrqPXS7XOd00Ci0ku2Fk'
-    COUNTRY = 'RU'
-    SALE = 50
+    COUNTRY = random.choice(['RU'])  # (['AU', 'DE', 'FI', 'AT', 'FR'])
+    SALE = 25
     CURRENCY = item.currency.lower()
-    tax = stripe.TaxRate.create(display_name="Sales Tax",
+    NAME = "Petr"
+
+    tax = stripe.TaxRate.create(display_name="НДС",
                                 inclusive=False,
-                                percentage=20,
-                                country="RU",
-                                description="НДС",
+                                percentage=7.25,
+                                country="US",
+                                description="US Sales Tax",
                                 )
+
     customer = stripe.Customer.create(description="Default User",
                                       address={'country': COUNTRY},
+                                      name=NAME,
                                       )
+
     product = stripe.Product.create(name=item.name,
                                     description=item.description,
                                     )
-    discount = stripe.Coupon.create(percent_off=SALE, duration="once")
+
+    discount = stripe.Coupon.create(percent_off=SALE,
+                                    duration="once",
+                                    )
+
     price = stripe.Price.create(
         product=product.id,
         unit_amount=100 * item.price,
         currency=item.currency.lower(),
         tax_behavior='exclusive',
     )
-    try:
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    'price': price,
-                    'quantity': 1,
-                },
-            ],
-            automatic_tax={
-                'enabled': True,
-            },
-            currency=CURRENCY,
-            discounts=[{
-                'coupon': discount,
-            }],
-            mode='payment',
-            customer=customer,
-            success_url='http://localhost:8000/success',
-            cancel_url='http://localhost:8000/cancel',
 
-        )
-        return checkout_session
-    except Exception as error:
-        return str(error)
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                'price': price,
+                'quantity': 1,
+                'tax_rates': [tax['id']],
+            },
+        ],
+        automatic_tax={
+            # Enable this and select country from ['AU', 'DE', 'FI', 'AT', 'FR'] for automated calc of taxes
+            'enabled': False,
+        },
+        currency=CURRENCY,
+        discounts=[{
+            'coupon': discount,
+        }],
+        mode='payment',
+        customer=customer,
+        success_url='http://localhost:8000/success',
+        cancel_url='http://localhost:8000/cancel',
+    )
+    return checkout_session
 
 
 def success(request):
