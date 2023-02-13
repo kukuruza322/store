244a1a2f7b5f58f93532c8ce512b5a0cb2f5f142
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2023-02-13 16:28:55 +0300

    env file ready

--- store/store/settings.py
+++ store/store/settings.py
@@ -9,23 +9,26 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import environ
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zxdvb-j)no*24mrb9)gwm9wqu$=7w2eeoduc+=182)s&qgl5dg'
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=[env('ALLOWED_HOSTS')]
+
+ROOT_URLCONF = env('ROOT_URLCONF')
 
 SESSION_SAVE_EVERY_REQUEST = True
 
@@ -51,7 +54,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'store.urls'
 
 TEMPLATES = [
     {
@@ -74,14 +76,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+        {'ENGINE': env('DB_ENGINE'),
+         'NAME': env('DB_NAME'),
+         }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -101,23 +101,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
+LANGUAGE_CODE = env('LANGUAGE_CODE')
 
-LANGUAGE_CODE = 'en-us'
+TIME_ZONE = env('TIME_ZONE')
 
-TIME_ZONE = 'UTC'
+USE_I18N = env('USE_I18N')
 
-USE_I18N = True
-
-USE_TZ = True
-
+USE_TZ = env('USE_TZ')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = env('STATIC_URL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
